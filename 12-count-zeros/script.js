da242f28bf18298c975653d0269e811a9f702f62
Rubacova Polina <rubacova.polina@yandex.ru>
2022-10-28 10:20:43 +0300

    12 finish

--- 12-count-zeros/script.js
+++ 12-count-zeros/script.js
@@ -7,10 +7,20 @@
 */
 
 function countZeros(n) {
-    // Напишите код здесь
+  let res = 0;
+  let str;
+  for (let i=1; i<=n; i++){
+    str = i.toString();
+    while (str.indexOf('0') > -1) {
+      res +=1;
+      str = str.slice(str.indexOf('0')+1);
+    }
+  }
+  return res;
 }
 
 // Протестируйте решение, вызывая функцию с разными аргументами:
 
-console.log(countZeros(20)); // 2 – два нуля, по одному в числах 10 и 20
-console.log(countZeros(100)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+//console.log(countZeros(20)); // 2 – два нуля, по одному в числах 10 и 20
+//console.log(countZeros(100)); // 11 – 11 нулей в числах: 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+console.log(countZeros(342));
